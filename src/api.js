1048d1f1efb45c20318eb38aa36dc398cbc10fba
ThaSMorato <tha.s.morato@gmail.com>
2021-09-23 23:42:53 -0300

    correct answer

--- src/api.js
+++ src/api.js
@@ -2,56 +2,85 @@ const http = require("http");
 const CarService = require("./service/carService");
 const { join } = require("path");
 
+const DEFAULT_PORT = 3000;
+const DEFAULT_HEADERS = {
+  "Content-Type": "application/json",
+};
+
 const carsDatabase = join(__dirname, "../database", "cars.json");
 
-const createCarService = () => new CarService({ car: carsDatabase });
+const createCarService = () => ({ carService: new CarService({ car: carsDatabase }) });
 
-const routes = {
-  default: (request, response) => {
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    response.write("Hello world");
-    return response.end();
-  },
-  "/avaiableCar:get": (request, response) => {
-    return response.end();
-  },
-  "/finalPrice:post": async (request, response) => {
-    for await (const data of request) {
-      const { customer, numberOfDays, carCategory } = JSON.parse(data);
-      const result = createCarService().calculateFinalPrice({
-        customer,
-        carCategory,
-        numberOfDays,
-      });
-      response.write(JSON.stringify({ price: result }));
-      return response.end();
-    }
-  },
-  "/rent:post": async (request, response) => {
-    for await (const data of request) {
-      const { customer, numberOfDays, carCategory } = JSON.parse(data);
-      const result = await createCarService().rent({
-        customer,
-        carCategory,
-        numberOfDays,
-      });
-      response.write(JSON.stringify(result));
-      return response.end();
-    }
-  },
-};
+class Api {
+  constructor(dependencies = createCarService()) {
+    this.carService = dependencies.carService;
+  }
 
-const handler = (request, response) => {
-  const { url, method } = request;
-  const routeKey = `${url}:${method.toLowerCase()}`;
+  createRoutes() {
+    return {
+      default: (request, response) => {
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        response.write("Hello world");
+        return response.end();
+      },
+      "/avaiableCar:post": async (request, response) => {
+        for await (const data of request) {
+          const { carCategory } = JSON.parse(data);
+          const result = this.carService.getAvailableCar(carCategory);
+          response.write(JSON.stringify({ result }));
+          return response.end();
+        }
+      },
+      "/finalPrice:post": async (request, response) => {
+        for await (const data of request) {
+          const { customer, numberOfDays, carCategory } = JSON.parse(data);
+          const result = this.carService.calculateFinalPrice({
+            customer,
+            carCategory,
+            numberOfDays,
+          });
+          response.write(JSON.stringify({ result }));
+          return response.end();
+        }
+      },
+      "/rent:post": async (request, response) => {
+        for await (const data of request) {
+          const { customer, numberOfDays, carCategory } = JSON.parse(data);
+          const result = await this.carService.rent({
+            customer,
+            carCategory,
+            numberOfDays,
+          });
+          response.write(JSON.stringify({ result }));
+          return response.end();
+        }
+      },
+    };
+  }
 
-  const chosen = routes[routeKey] || routes.default;
+  handler(request, response) {
+    const { url, method } = request;
+    const routeKey = `${url}:${method.toLowerCase()}`;
 
-  response.writeHeader(200, { "Content-Type": "application/json" });
+    const routes = this.createRoutes();
+    const chosen = routes[routeKey] || routes.default;
 
-  return chosen(request, response);
-};
+    response.writeHeader(200, DEFAULT_HEADERS);
 
-const app = http.createServer(handler).listen(3000, () => console.log("teste"));
+    return chosen(request, response);
+  }
 
-module.exports = app;
+  createServer(port = DEFAULT_PORT) {
+    const app = http.createServer(this.handler.bind(this)).listen(port, () => console.log("teste"));
+
+    return app;
+  }
+}
+
+// adiciono NODE_ENV para teste (adicionado no Package.json)
+if (process.env.NODE_ENV !== "test") {
+  const api = new Api();
+  api.createServer();
+}
+
+module.exports = (dependencies) => new Api(dependencies);
--- test/e2e/api.test.js
+++ test/e2e/api.test.js
@@ -1,8 +1,12 @@
-const { expect } = require("chai");
 const { describe, it, before, beforeEach, after, afterEach } = require("mocha");
+const { expect } = require("chai");
 const request = require("supertest");
-const app = require("../../src/api");
-const Transaction = require("../../src/entities/transaction");
+const sinon = require("sinon");
+
+const { join } = require("path");
+
+const CarService = require("./../../src/service/carService");
+const SERVER_TEST_PORT = 4000;
 
 const mocks = {
   validCarCategory: require("../mocks/valid-car-category.json"),
@@ -10,82 +14,158 @@ const mocks = {
   validCustomer: require("../mocks/valid-customer.json"),
 };
 
+const carsDatabase = join(__dirname, "../../database", "cars.json");
+
 describe("API SUIT TEST", () => {
+  let app = {};
+  let sandbox = {};
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  before(() => {
+    const api = require("../../src/api");
+    const carService = new CarService({
+      cars: carsDatabase,
+    });
+    const instance = api({ carService });
+
+    app = {
+      instance,
+      server: instance.createServer(SERVER_TEST_PORT),
+    };
+  });
+
   describe("/default", () => {
     it("should request the default and return HTTP status 200", async () => {
-      await request(app).get("/default").expect(200);
+      await request(app.server).get("/default").expect(200);
     });
     it("should request the default and return content-type html", async () => {
-      await request(app).get("/default").expect("Content-Type", /html/);
+      await request(app.server).get("/default").expect("Content-Type", /html/);
     });
   });
   describe("/avaiableCar", () => {
     it("should request the avaiableCar and return HTTP status 200", async () => {
-      await request(app).get("/avaiableCar").expect(200);
+      await request(app.server).post("/avaiableCar").expect(200).done;
     });
     it("should request the avaiableCar and return content-type json", async () => {
-      await request(app).get("/avaiableCar").expect("Content-Type", /json/);
+      await request(app.server).post("/avaiableCar").expect("Content-Type", /json/).done;
+    });
+    it("Given a carCategory, it should return a random car", async () => {
+      const car = { ...mocks.validCar };
+      const carCategory = {
+        ...mocks.validCarCategory,
+        carIds: [car.id],
+      };
+
+      sandbox
+        .stub(app.instance.carService, app.instance.carService.getRandomPositionFromArray.name)
+        .returns(0);
+
+      const expected = {
+        result: car,
+      };
+
+      await request(app.server).post("/avaiableCar").send({ carCategory }).expect(expected).done;
     });
   });
   describe("/finalPrice", () => {
     it("should send the finalPrice and return HTTP status 200", async () => {
-      await request(app).post("/finalPrice").expect(200).done;
+      await request(app.server).post("/finalPrice").expect(200).done;
     });
     it("should send the finalPrice and return content-type json", async () => {
-      await request(app).post("/finalPrice").expect("Content-Type", /json/).done;
+      await request(app.server).post("/finalPrice").expect("Content-Type", /json/).done;
     });
     it("should send the finalPrice the params and get back the price", async () => {
-      const customer = Object.create(mocks.validCustomer);
+      const car = { ...mocks.validCar };
+      const customer = {
+        ...mocks.validCustomer,
+        age: 50,
+      };
 
-      customer.age = 50;
-
-      const carCategory = Object.create(mocks.validCarCategory);
-
-      carCategory.price = 37.6;
+      const carCategory = {
+        ...mocks.validCarCategory,
+        price: 37.6,
+      };
 
       const numberOfDays = 5;
 
-      await request(app)
-        .post("/finalPrice")
-        .send({ customer, numberOfDays, carCategory })
-        .expect({ price: "R$ 244,40" }).done;
+      const body = {
+        customer,
+        carCategory,
+        numberOfDays,
+      };
+
+      sandbox
+        .stub(
+          app.instance.carService.carRepository,
+          app.instance.carService.carRepository.find.name
+        )
+        .resolves(car);
+
+      const expected = {
+        result: app.instance.carService.currencyFormat.format(244.4),
+      };
+
+      await request(app.server).post("/finalPrice").send(body).expect(expected).done;
     });
   });
   describe("/rent", () => {
     it("should send the rent and return HTTP status 200", async () => {
-      await request(app).post("/rent").expect(200).done;
+      await request(app.server).post("/rent").expect(200).done;
     });
     it("should send the rent and return content-type json", async () => {
-      await request(app).post("/rent").expect("Content-Type", /json/).done;
+      await request(app.server).post("/rent").expect("Content-Type", /json/).done;
     });
     it("should send the rent the params and get back the transaction", async () => {
       const car = mocks.validCar;
+      const carCategory = {
+        ...mocks.validCarCategory,
+        price: 37.6,
+        carIds: [car.id],
+      };
 
-      const customer = Object.create(mocks.validCustomer);
-
-      customer.age = 20;
-
-      const carCategory = { ...mocks.validCarCategory, price: 37.6, carsId: [car.id] };
+      const customer = {
+        ...mocks.validCustomer,
+        age: 20,
+      };
 
       const numberOfDays = 5;
 
-      const now = new Date();
-      now.setDate(now.getTime() + numberOfDays);
+      const expectedAmount = app.instance.carService.currencyFormat.format(206.8);
+      const dueDate = "10 de novembro de 2020";
 
-      const options = {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
+      const body = {
+        customer,
+        carCategory,
+        numberOfDays,
       };
 
-      const dueDate = now.toLocaleDateString("pt-br", options);
+      const now = new Date(2020, 10, 5);
+      sandbox.useFakeTimers(now.getTime());
 
-      await request(app)
-        .post("/rent")
-        .send({ customer, numberOfDays, carCategory })
-        .then(({ body }) => {
-          expect(typeof body).to.be.equal(typeof new Transaction());
-        });
+      sandbox
+        .stub(
+          app.instance.carService.carRepository,
+          app.instance.carService.carRepository.find.name
+        )
+        .resolves(car);
+
+      const expected = {
+        result: {
+          customer,
+          car,
+          amount: expectedAmount,
+          dueDate,
+        },
+      };
+
+      await request(app.server).post("/rent").send(body).expect(expected).done;
     });
   });
 });
